1d2171e559a388e830c1a5cb2c25f0e4c2834cca
ybchen1 <ybchen1@chinaums.com>
2020-11-05 18:43:55 +0800

    leetcode1:TwoSum

--- Code01TwoSum.java
+++ Code01TwoSum.java
@@ -1,3 +1,53 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: ybchen
+ * @Date: 2020/11/5 17:30
+ */
 public class Code01TwoSum {
+    /**
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/two-sum
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length < 2) {
+            return nums;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+
+        }
+        return new int[]{};
+    }
+
+
+    public int[] twoSum2(int[] nums, int target) {
+        if (nums == null || nums.length < 2) {
+            return nums;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{j, i};
+                }
+            }
+        }
+        return new int[]{};
+
+    }
+
 
 }
