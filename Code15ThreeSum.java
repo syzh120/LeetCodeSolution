4334a84486bc71c4a89dffc7fd5741925105b8c9
ybchen1 <ybchen1@chinaums.com>
2020-11-10 18:20:04 +0800

    code15 threeSum brute force way

---
+++ Code15ThreeSum.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Author: ybchen
+ * @Date: 2020/11/9 18:23
+ */
+public class Code15ThreeSum {
+
+    /**
+     *给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+     *
+     * 注意：答案中不可以包含重复的三元组。
+     *
+     *  
+     *
+     * 示例：
+     *
+     * 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+     *
+     * 满足要求的三元组集合为：
+     * [
+     *   [-1, 0, 1],
+     *   [-1, -1, 2]
+     * ]
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/3sum
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param nums
+     * @return
+     */
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> resList = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        boolean[] visited = new boolean[nums.length];
+        process(nums, 0, path, resList, visited, 0);
+        return resList;
+    }
+
+    public static void process(int[] nums, int startIndex, List<Integer> path, List<List<Integer>> resList, boolean[] visited, int res) {
+        for (int i = startIndex; i < nums.length; i++) {
+
+            if(visited[i]==true){
+                continue;
+            }
+            if (i > 0 && nums[i] == nums[i - 1] && visited[i-1] == false) {
+                continue;
+            }
+            if(path.size()>3) continue;
+            visited[i] = true;
+            res += nums[i];
+            path.add(nums[i]);
+            if (path.size() == 3 && res == 0) {
+                resList.add(new ArrayList<>(path));// deep copy
+            }
+            process(nums, i + 1, path, resList, visited, res);
+            res = res - nums[i];
+            path.remove(path.size() - 1);
+            visited[i] = false;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-4, -1, -1, 0, 1, 2};
+        List<List<Integer>> resList = threeSum(nums);
+        for (List<Integer> res : resList) {
+            for (Integer num : res) {
+                System.out.print(num);
+            }
+            System.out.println();
+        }
+    }
+
+
+}
