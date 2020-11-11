f2e17f16ece5691a976415188d01e4d2b5994290
ybchen1 <ybchen1@chinaums.com>
2020-11-11 16:22:09 +0800

    Code18FourSum :modify the corner case

--- Code15ThreeSum.java
+++ Code15ThreeSum.java
@@ -86,7 +86,7 @@ public class Code15ThreeSum {
             return resList;
         }
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length-2; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
