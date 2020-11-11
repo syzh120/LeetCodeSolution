39fb3721aed611421188379f16f7d324f366dba4
ybchen1 <ybchen1@chinaums.com>
2020-11-11 15:50:01 +0800

    Code18FourSum

---
+++ Code18FourSum.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Author: ybchen
+ * @Date: 2020/11/11 15:05
+ */
+public class Code18FourSum {
+
+    /**
+     * 给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，使得 a + b + c + d 的值与 target 相等？找出所有满足条件且不重复的四元组。
+     *
+     * 注意：
+     *
+     * 答案中不可以包含重复的四元组。
+     *
+     * 示例：
+     *
+     * 给定数组 nums = [1, 0, -1, 0, -2, 2]，和 target = 0。
+     *
+     * 满足要求的四元组集合为：
+     * [
+     *   [-1,  0, 0, 1],
+     *   [-2, -1, 1, 2],
+     *   [-2,  0, 0, 2]
+     * ]
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/4sum
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param nums
+     * @param target
+     * @return
+     */
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> resList = new ArrayList<>();
+        if (nums == null || nums.length < 4) {
+            return resList;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < nums.length; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int k = j + 1;
+                int l = nums.length - 1;
+                while (k < l) {
+                    if (k > j + 1 && nums[k] == nums[k - 1]) {
+                        k = k + 1;
+                        continue;
+                    }
+                    int sum = nums[i] + nums[j] + nums[k] + nums[l];
+                    if (sum == target) {
+                        resList.add(Arrays.asList(new Integer[]{nums[i], nums[j], nums[k], nums[l]}));
+                        l--;
+                        k++;
+                    } else if (target > sum) {
+                        k++;
+                    } else {
+                        l--;
+                    }
+                }
+
+            }
+        }
+        return resList;
+    }
+}
