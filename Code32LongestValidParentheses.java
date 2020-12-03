d44092c63cb7a4d960621296b1dbcaf07cd0b3df
ybchen1 <ybchen1@chinaums.com>
2020-12-03 16:48:18 +0800

    code32:sliding window

--- Code32LongestValidParentheses.java
+++ Code32LongestValidParentheses.java
@@ -4,6 +4,28 @@
  */
 public class Code32LongestValidParentheses {
 
+    /**
+     * Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+     *
+     * Example 1:
+     *
+     * Input: s = "(()"
+     * Output: 2
+     * Explanation: The longest valid parentheses substring is "()".
+     * Example 2:
+     *
+     * Input: s = ")()())"
+     * Output: 4
+     * Explanation: The longest valid parentheses substring is "()()".
+     * Example 3:
+     *
+     * Input: s = ""
+     * Output: 0
+     * @param s
+     * @return
+     */
+
+    //TODO:add dynamic programming approach
     public static int longestValidParentheses(String s) {
         char[] chs = s.toCharArray();
         int maxLen = getMaxLen(chs, '(');
