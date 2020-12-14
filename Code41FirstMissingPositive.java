e194b998871802176884aa29454f669bda1f3b8f
ybchen1 <ybchen1@chinaums.com>
2020-12-14 17:02:06 +0800

    CODE41 CODE43

---
+++ Code41FirstMissingPositive.java
@@ -0,0 +1,35 @@
+/**
+ * @Author: ybchen
+ * @Date: 2020/12/14 15:55
+ */
+public class Code41FirstMissingPositive {
+
+    public static int firstMissingPositive(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] > 0 && nums[i] <= nums.length && nums[i] != i + 1 && nums[nums[i] - 1] != nums[i]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return nums.length + 1;
+    }
+
+
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1};
+        int result = firstMissingPositive(nums);
+        System.out.println(result);
+    }
+}
---
+++ Code43Multiply.java
@@ -0,0 +1,8 @@
+/**
+ * @Author: ybchen
+ * @Date: 2020/12/14 15:24
+ */
+public class Code43Multiply {
+
+
+}
