90e589c54793c12d78a762da9f34b854535b6abd
ybchen1 <ybchen1@chinaums.com>
2020-12-24 15:27:07 +0800

    add description

--- Code98ValidateBinarySearchTree.java
+++ Code98ValidateBinarySearchTree.java
@@ -23,6 +23,18 @@ public class Code98ValidateBinarySearchTree {
         }
     }
 
+    /**
+     * Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+     *
+     * <p>A valid BST is defined as follows:
+     *
+     * <p>The left subtree of a node contains only nodes with keys less than the node's key. The right
+     * subtree of a node contains only nodes with keys greater than the node's key. Both the left and
+     * right subtrees must also be binary search trees
+     *
+     * @param root
+     * @return
+     */
     public boolean isValidBST(TreeNode root) {
         return process(root).isBst;
     }
