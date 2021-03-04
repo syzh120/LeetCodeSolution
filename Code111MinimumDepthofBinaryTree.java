2f9df7aca673d1f9a30fbdd6626a0fbd7613e772
ybchen1 <ybchen1@chinaums.com>
2021-03-04 14:19:41 +0800

    add code111 problem

--- Code111MinimumDepthofBinaryTree.java
+++ Code111MinimumDepthofBinaryTree.java
@@ -3,6 +3,35 @@
  * @Date: 2021/3/4 14:08
  */
 public class Code111MinimumDepthofBinaryTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
 
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = minDepth(root.left);
+        int rightDepth = minDepth(root.right);
+        if (leftDepth == 0 || rightDepth == 0) {
+            return leftDepth == 0 ? rightDepth + 1 : leftDepth + 1;
+        }
+        return Math.min(leftDepth, rightDepth) + 1;
+    }
 
 }
